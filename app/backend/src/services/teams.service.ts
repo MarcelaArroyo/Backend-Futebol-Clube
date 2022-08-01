b0d16ec3ff07cf5245a3d6dfdb791b87bf46780c
Marcela Arroyo <malvesarroyo@gmail.com>
2022-08-01 14:57:03 -0300

    refactor: removing the map function

--- app/backend/src/services/teams.service.ts
+++ app/backend/src/services/teams.service.ts
@@ -1,27 +1,17 @@
 import Teams from '../database/models/teams';
-import ITeam from '../interfaces/ITeam';
 
-const getAllTeams = async (): Promise<ITeam[]> => {
+const getAllTeams = async (): Promise<Teams[]> => {
   const teams = await Teams.findAll();
-  const result: ITeam[] = teams.map((team) => {
-    return {
-      id: +team.id,
-      teamName: team.team_name,
-    }
-  });
 
-  return result as ITeam[]
+  return teams;
 }
 
-const getTeamById = async (id: number): Promise<ITeam> => {
+const getTeamById = async (id: number): Promise<Teams | null> => {
   const team = await Teams.findOne({
     where: { id }
   });
-  
-  return {
-    id: team?.id,
-    teamName: team?.team_name
-  } as ITeam
+
+  return team;
 }
 
 export default {
